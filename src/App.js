d5d44d259c64fd1562f8224b644611533695c194
Naim <naimansary92@gmail.com>
2021-12-26 14:26:38 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
+
+import { Container, Row, Col } from "reactstrap";
+import { ToastContainer, toast } from "react-toastify";
+import BuyPage from "./Components/BuyPage";
+import Cart from "./Components/Cart";
 
 function App() {
+  const [cartItem, setCartItem] = useState([]);
+
+  //method to add item to cart and check whether already added or not
+  const addInCart = (item) => {
+    const isAlreadyAdded = cartItem.findIndex(function (array) {
+      return array.id === item.id;
+    });
+    if (isAlreadyAdded !== -1) {
+      toast("Already Added in Cart", {
+        type: "error",
+      });
+      return;
+    }
+
+    setCartItem([...cartItem, item]);
+  };
+
+  //method to buy the items
+
+  const buyNow = () => {
+    setCartItem([]);
+
+    toast("Purchase Complete", {
+      type: "success",
+    });
+  };
+
+  //method to remove items from cart
+
+  const removeItem = (item) => {
+    setCartItem(cartItem.filter((singleItem) => singleItem.id !== item.id));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid>
+      <ToastContainer />
+      <Row>
+        <Col md="8">
+          <BuyPage addInCart={addInCart} />
+        </Col>
+        <Col md="4">
+          <Cart cartItem={cartItem} removeItem={removeItem} buyNow={buyNow} />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
